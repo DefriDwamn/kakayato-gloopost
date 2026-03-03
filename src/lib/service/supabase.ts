d562ed6660b901364aed475158f909ec8b3020b1
Яков Новашинский Михайлович <72817400+DefriDwamn@users.noreply.github.com>
2026-03-03 10:13:14 +0300

    deps: update env key name 2

--- src/lib/service/supabase.ts
+++ src/lib/service/supabase.ts
@@ -2,7 +2,7 @@ import Context from '$lib/util/context';
 import { createClient, type SupabaseClient } from '@supabase/supabase-js';
 
 export const defaultClient = createClient(
-	import.meta.env.VITE_SUPABASE_PROJECT_URL,
+	import.meta.env.VITE_SUPABASE_DATABASE_URL,
 	import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
