4e8b8f211657fccde1f297dafc4ea955fdacca46
Yakov Novashinskii <Emailno@bk.ru>
2026-03-03 11:51:55 +0300

    feat: supabase fix goes brr

--- supabase/functions/_shared/similarity.ts
+++ supabase/functions/_shared/similarity.ts
@@ -1,4 +1,18 @@
-export async function loadSimilarity() {
-	const data = await Deno.readTextFile('../_shared/similarity.txt');
-	return data.split('\n').map((line) => line.split(',').map(Number.parseFloat));
+import type { SupabaseClient } from 'jsr:@supabase/supabase-js@2';
+
+export async function loadSimilarity(supabase: SupabaseClient) {
+	const { data, error } = await supabase
+		.storage
+		.from('similarity')
+		.download('similarity.txt');
+
+	if (error) throw error;
+	if (!data) throw new Error('similarity.txt is empty');
+
+	const text = await data.text();
+
+	return text
+		.trim()
+		.split('\n')
+		.map((line) => line.split(',').map((x) => Number.parseFloat(x)));
 }
--- supabase/functions/recommend/index.ts
+++ supabase/functions/recommend/index.ts
@@ -1,23 +1,20 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
-import { loadFamousBooks } from './../_shared/famous.ts';
-import { loadSimilarity } from './../_shared/similarity.ts';
+import { loadFamousBooks } from '../_shared/famous.ts';
+import { loadSimilarity } from '../_shared/similarity.ts';
 import { response, serve } from '../_shared/server.ts';
 
 serve(async (_, supabase) => {
 	const { data, error } = await supabase.from('books').select();
 
-	if (error != null) {
-		throw error;
-	}
+	if (error) throw error;
 
 	const books = await loadFamousBooks();
+
 	const notViewed = Object.keys(books)
 		.map((key) => Number.parseInt(key))
 		.filter((id) => data.every(({ book_id }) => book_id !== id));
 
-	if (notViewed.length === 0) {
-		throw new Error('all books viewed');
-	}
+	if (notViewed.length === 0) throw new Error('all books viewed');
 
 	const liked = data.filter((book) => book.will_read === true);
 
@@ -27,11 +24,13 @@ serve(async (_, supabase) => {
 	}
 
 	const source = liked[Math.floor(Math.random() * liked.length)].book_id;
-	const similarity = await loadSimilarity();
+
+	const similarity = await loadSimilarity(supabase);
+
 	const similar = similarity[source]
 		.map((val, index) => ({ val, index }))
 		.sort((a, b) => b.val - a.val)
 		.find(({ index }) => notViewed.includes(index));
 
-	return response({ id: similar.index, ...books[similar.index] });
+	return response({ id: similar!.index, ...books[similar!.index] });
 });
